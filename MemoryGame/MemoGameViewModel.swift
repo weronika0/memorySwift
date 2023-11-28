e817f3a92c6d147cbf4f0e6e1334fce39b8aa332
Weronika Nowak <weronika.nowak@pollub.edu.pl>
2023-11-28 06:34:56 -0800

    nie zmienia motywu

---
+++ MemoryGame/MemoGameViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MemoGameViewModel.swift
+//  MemoryGame
+//
+//  Created by student on 28/11/2023.
+//
+
+import SwiftUI
+
+struct MemoGameViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemoGameViewModel()
+}
--- MemoryGame/MemoryGameApp.swift
+++ MemoryGame/MemoryGameApp.swift
@@ -2,16 +2,7 @@
 //  MemoryGameApp.swift
 //  MemoryGame
 //
-//  Created by student on 11/10/2023.
+//  Created by student on 28/11/2023.
 //
 
-import SwiftUI
-
-@main
-struct MemoryGameApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
+import Foundation
